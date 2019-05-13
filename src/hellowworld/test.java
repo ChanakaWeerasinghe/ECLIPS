039d6d7b47e8187046c4ea9a3082d0be460317b2
chanakaweerasinghe <chanakaweerasinghe92@gmail.com>
2019-05-13 00:51:51 -0700

    now

--- src/hellowworld/test.java
+++ src/hellowworld/test.java
@@ -5,7 +5,6 @@ public class test {
 	public static void main(String [] args)
 	{
 	System.out.print("hellow ");
-	git remote add origin https://github.com/ChanakaWeerasinghe/ECLIPS.git
-		git push -u origin master	 
+	int a=10;	 
 	}
 }
